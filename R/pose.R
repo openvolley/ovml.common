f4feb5fb05d5635597e8f21743ac6533e2aab124
Ben Raymond <ben@theraymonds.org>
2022-07-27 09:53:53 +1000

    catch zero-row input

--- R/pose.R
+++ R/pose.R
@@ -11,7 +11,7 @@
 #'
 #' @export
 process_pose_dets <- function(pose, original_w, original_h, input_image_size, as = "segments", letterboxing = FALSE) {
-
+    if (nrow(pose) < 1) return(NULL)
     skeleton <- list(c(16, 14), c(14, 12), c(17, 15), c(15, 13), c(12, 13), c(6, 12),
                      c(7, 13), c(6, 7), c(6, 8), c(7, 9), c(8, 10), c(9, 11), c(2, 3),
                      c(1, 2), c(1, 3), c(2, 4), c(3, 5), c(4, 6), c(5, 7))
